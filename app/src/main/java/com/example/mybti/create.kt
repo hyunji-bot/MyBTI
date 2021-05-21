5d1828a0f863460495f77049693d860203c27278
DESKTOP-3RE8M4L\User <iceteateemo@gmail.com>
2021-05-21 14:34:16 +0900

    asdf

---
+++ app/src/main/java/com/example/mybti/create.kt
@@ -0,0 +1,11 @@
+package com.example.mybti
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class create : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create)
+    }
+}
