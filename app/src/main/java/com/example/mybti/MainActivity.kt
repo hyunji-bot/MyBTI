5e421b58cabcf5b5c191b1936e50651db2d1965d
DESKTOP-3RE8M4L\User <iceteateemo@gmail.com>
2021-05-24 15:00:45 +0900

    main -> create -> create_qna 화면 구성, 이동

--- app/src/main/java/com/example/mybti/MainActivity.kt
+++ app/src/main/java/com/example/mybti/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.mybti
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_create.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val intent = Intent(this, create::class.java) // 인텐트를 생성
+
+        button6.setOnClickListener { // 버튼 클릭시 할 행동
+            startActivity(intent)  // 화면 전환하기
+        }
     }
 }
--- app/src/main/java/com/example/mybti/create_qna.kt
+++ app/src/main/java/com/example/mybti/create_qna.kt
@@ -1,11 +1,19 @@
 package com.example.mybti
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_create_qna.*
 
 class create_qna : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_qna)
+
+//        val intent = Intent(this, ::class.java) // 인텐트를 생성
+
+//        nextbtn.setOnClickListener { // 버튼 클릭시 할 행동
+//            startActivity(intent)  // 화면 전환하기
+//        }
     }
 }
